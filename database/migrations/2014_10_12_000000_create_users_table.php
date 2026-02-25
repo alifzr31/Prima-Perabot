f3d82a50dec533f8fd203551dd823447527eb41f
Alif ZR <alifzakya.r@gmail.com>
2026-02-26 00:16:34 +0700

    .

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,7 +20,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('phone')->unique();
             $table->string('password');
-            $table->enum('role', ['superadmin', 'admin', 'user'])->default('user');
+            $table->enum('role', ['superadmin', 'admin', 'customer'])->default('customer');
             $table->rememberToken();
             $table->timestamps();
         });
